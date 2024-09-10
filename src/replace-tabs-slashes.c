a38c9bad629acb20f9e898d3abc6231353544fcd
Asadbek Raimov <asadbekraimov642@gmail.com>
2024-09-10 17:53:47 +0500

    [+] Replace tabs, backspaces and slashes

---
+++ src/replace-tabs-slashes.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        switch (c)
+        {
+        case '\t':
+            printf("\\t");
+            break;
+        case '\b':
+            printf("\\b");
+            break;
+        case '\\':
+            printf("\\\\");
+            break;
+        default:
+            putchar(c);
+            break;
+        }
+    }
+}
